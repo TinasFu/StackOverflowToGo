e77cf9bdfb7438ca4daf9c530ae72338486fb7d6
Tina Fu <TinasFu@outlook.com>
2014-11-10 13:50:46 -0800

    Initial Commit

---
+++ StackOverflowToGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverflowToGo
+//
+//  Created by Shiquan Fu on 11/10/14.
+//  Copyright (c) 2014 Tina Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverflowToGo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackOverflowToGo
+//
+//  Created by Shiquan Fu on 11/10/14.
+//  Copyright (c) 2014 Tina Fu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
